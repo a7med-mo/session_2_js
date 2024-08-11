b11f898e284353c4d3c2e35aa1bb65e0ba63c8f1
ahmed mohammed <ahmed.c7.mo@gmail.com>
2024-08-11 20:35:33 +0300

    task

---
+++ task/script.js
@@ -0,0 +1,145 @@
+
+/*
+1- Check if a given number is less than or equal to 100
+If number is less than or equal to 100
+Console.log number is less than or equal to 100
+Else number is greater than 100
+*/
+
+// let num = prompt("Enter the number:");
+
+// if (num <= 100) {
+//     console.log("The number is less than or equal to 100");
+// } else {
+//     console.log("The number is greater than 100");
+// }
+
+// ============================================================================================
+
+/*
+2- Check the number of characters in text
+If number of characters greater than 100 
+Console log the string is long 
+Else console log string is short 
+*/
+
+// let text = prompt("Enter the text:");
+
+// if (text.length > 100) {
+//     console.log("The string is long");
+// } else {
+//     console.log("The string is short");
+// }
+
+// ============================================================================================
+
+/*
+3- Password validation 
+If password equal confirm password 
+Password is validation else no valied 
+*/
+
+// pass = prompt("Enter the password");
+// confirm_pass = prompt("Confirm Password");
+
+// if (pass == confirm_pass) {
+//     console.log("Passwords match");
+// } else {
+//     console.log("Passwords do not match");
+// }
+
+
+// ============================================================================================
+
+/*
+4- Check the day of the week if saterday 
+It's weekend else work day
+ */
+
+// today = prompt("Enter the today").toLowerCase()
+
+// if (today === "saturday") {
+//     console.log("Today is a weekend");
+// } else {
+//     console.log("Today is a weekday");
+// }
+
+// ============================================================================================
+
+// 5- Check number positive or negative
+
+// number = prompt("Enter the number");
+
+// if (number >= 0) {
+//     console.log("Number is positive");
+// } else {
+//     console.log("Number is negative");
+// }
+
+// ============================================================================================
+
+/*
+6- Verifying academic grade less than 50 F
+Greater 60 it's D 70 C 80 B greater than it A
+*/
+
+// grade = prompt("Enter the grade");
+
+// if (grade < 50) {
+//     console.log("F");
+// } else if (grade > 50 && grade < 60) {
+//     console.log("D")
+// } else if (grade > 60 && grade < 70) {
+//     console.log("C")
+// } else if (grade > 70 && grade < 80) {
+//     console.log("B")
+// } else if (grade > 80 && grade < 100) {
+//     console.log("A")
+// } else {
+//     console.log("Invalid grade")
+// }
+
+// ===============================================================================================
+
+/*
+7- Check temperature temp greater than 30 it's hot 
+else it's cold
+ */
+
+// temp = prompt("Enter the temperature");
+
+// if (temp > 30) {
+//     console.log("It's hot");
+// } else {
+//     console.log("It's cold");
+// }
+
+// ===============================================================================================
+
+/*
+8- Checking an odd or even number
+ */
+
+// nummber = prompt("Enter the number");
+
+// if (nummber % 2 == 0) {
+//     console.log("The number is even");
+// } else {
+//     console.log("The number is odd");
+// }
+
+// ===============================================================================================
+
+/*
+9 - Checking an odd or even number if greater than 20 
+console log You can enter the university
+Else You can't enter
+*/
+
+// age = prompt("Enter the  age");
+
+// if (age > 20) { 
+//     console.log("You can enter the university")
+// } else {
+//     console.log("You can't enter the university")
+// }
